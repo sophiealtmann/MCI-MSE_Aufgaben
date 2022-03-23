6f7d90be1f28c330469660702a7bca8c184cc367
sophiealtmann <s.altmann@mci4me.at>
2022-03-23 12:10:28 +0100

    for.Schleife implementiert

--- PÜ2/example_read_data.py
+++ PÜ2/example_read_data.py
@@ -2,16 +2,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# %% Öffnen der Datei und konvertieren zu numpy-Array
-file_name =  'input_data/power_data_1.txt'
-power_data_watts = open(file_name).read().split("\n")
-x = np.array(power_data_watts)
+# %% Öffnen der Datei und konvertieren zu numpy-Array und Erstellen des Plots
+for i in range(1,4):
+    file_name =  'input_data/power_data_' + str(i) + '.txt'
+    power_data_watts = open(file_name).read().split("\n")
+    x = np.array(power_data_watts)
+    plt.title("Line graph"+str(i))
+    plt.plot(x, color="red")
+    plt.show()
 
-# %% Erstellen des Plots
-plt.title("Line graph")
-plt.plot(x, color="red")
 
-plt.show()
 
 
 # %%
