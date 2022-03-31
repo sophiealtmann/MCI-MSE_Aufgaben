9926709d98d9b1f8b4aba175059ba49b6cb86cef
Julian Huber <94922106+jhumci@users.noreply.github.com>
2022-03-31 16:17:47 +0200

    Aufgabe 2.2

--- PÜ2/example_read_data.py
+++ PÜ2/example_read_data.py
@@ -8,7 +8,7 @@ for i in range(1,4):
     file_name =  'input_data/power_data_' + str(i) + '.txt'
     power_data_watts = open(file_name).read().split("\n")
     x = np.array(power_data_watts)
-    plt.title("Line graph"+str(i))
+    plt.title("Leistungsdaten in Watt"+str(i))
     plt.plot(x, color="red")
     plt.show()
 
